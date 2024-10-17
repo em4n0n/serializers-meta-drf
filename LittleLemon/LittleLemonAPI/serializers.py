dcdcb76a54affcb52c8b24fbd683b7fb92adb35c
em4n0n <58372945+em4n0n@users.noreply.github.com>
2024-10-16 19:54:53 -0700

    Add a function to calculate tax

--- LittleLemon/LittleLemonAPI/serializers.py
+++ LittleLemon/LittleLemonAPI/serializers.py
@@ -1,5 +1,6 @@
 from rest_framework import serializers
 from .models import MenuItem
+from decimal import Decimal
 # class MenuItemSerializer(serializers.Serializer):
 #     id = serializers.IntegerField()
 #     title = serializers.CharField(max_length=255)
@@ -8,6 +9,10 @@ from .models import MenuItem
 
 class MenuItemSerializer(serializers.ModelSerializer):
     stock = serializers.IntegerField(source='inventory')
+    price_after_tax = serializers.SerializerMethodField(method_name = 'calculate_tax')
     class Meta:
         model = MenuItem
-        fields = ['id', 'title', 'price', 'inventory', 'stock']
+        fields = ['id', 'title', 'price', 'inventory', 'stock', 'price_after_tax']
+        
+def calculate_tax(self, product:MenuItem):
+    return product.price * Decimal(1.1)
