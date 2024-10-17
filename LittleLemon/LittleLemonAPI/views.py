fff8a00d48df0a94fafcb6a0dfbfcce9e5399792
em4n0n <58372945+em4n0n@users.noreply.github.com>
2024-10-16 19:37:29 -0700

    Create a function for single items

--- LittleLemon/LittleLemonAPI/views.py
+++ LittleLemon/LittleLemonAPI/views.py
@@ -1,9 +1,15 @@
 from rest_framework import Response
 from rest_framework.decorators import api_view
 from .models import MenuItem
-
+from .serializers import MenuItemSerializer
 # Create your views here.
 @api_view()
 def menu_items(request):
     items = MenuItem.objects.all
-    return Response(items.values())
+    serialized_item = MenuItemSerializer(items, many=True)
+    return Response(serialized_item.data)
+
+def single_item(request, id):
+    item = MenuItem.objects.get(pk=id)
+    serialized_item = MenuItemSerializer(item)
+    return Response(serialized_item.data)
