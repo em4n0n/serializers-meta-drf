bf9aee240d588f0fa6761309808243048406849e
em4n0n <58372945+em4n0n@users.noreply.github.com>
2024-10-16 19:03:12 -0700

    Create a function to fetch menu items

--- LittleLemon/LittleLemonAPI/views.py
+++ LittleLemon/LittleLemonAPI/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from rest_framework import Response
+from rest_framework.decorators import api_view
+from .models import MenuItem
 
 # Create your views here.
+@api_view()
+def menu_items(request):
+    items = MenuItem.objects.all
+    return Response(items.values())
