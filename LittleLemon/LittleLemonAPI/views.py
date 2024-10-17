315a770c6e954c2026e38c3b082d5c93082a6d6f
em4n0n <58372945+em4n0n@users.noreply.github.com>
2024-10-17 15:48:59 -0700

    Add category to the MenuItem list

--- LittleLemon/LittleLemonAPI/views.py
+++ LittleLemon/LittleLemonAPI/views.py
@@ -1,15 +1,16 @@
-from rest_framework import Response
+from rest_framework.response import Response
 from rest_framework.decorators import api_view
 from .models import MenuItem
 from .serializers import MenuItemSerializer
+from django.shortcuts import get_object_or_404
 # Create your views here.
 @api_view()
 def menu_items(request):
-    items = MenuItem.objects.all
+    items = MenuItem.objects.select_related('category').all()
     serialized_item = MenuItemSerializer(items, many=True)
     return Response(serialized_item.data)
 
 def single_item(request, id):
-    item = MenuItem.objects.get(pk=id)
+    item = MenuItem.objects.get(MenuItem, pk=id)
     serialized_item = MenuItemSerializer(item)
     return Response(serialized_item.data)
