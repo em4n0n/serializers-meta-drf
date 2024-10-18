6e18c4f3bbf0e41e75d983615bad065e013c3637
em4n0n <58372945+em4n0n@users.noreply.github.com>
2024-10-17 17:41:44 -0700

    Add StringRelatedField method

--- LittleLemon/LittleLemonAPI/serializers.py
+++ LittleLemon/LittleLemonAPI/serializers.py
@@ -1,6 +1,7 @@
 from rest_framework import serializers
 from .models import MenuItem
 from decimal import Decimal
+from .models import Category
 # class MenuItemSerializer(serializers.Serializer):
 #     id = serializers.IntegerField()
 #     title = serializers.CharField(max_length=255)
@@ -10,6 +11,7 @@ from decimal import Decimal
 class MenuItemSerializer(serializers.ModelSerializer):
     stock = serializers.IntegerField(source='inventory')
     price_after_tax = serializers.SerializerMethodField(method_name = 'calculate_tax')
+    category = serializers.StringRelatedField()
     class Meta:
         model = MenuItem
         fields = ['id', 'title', 'price', 'stock', 'price_after_tax', 'category']
