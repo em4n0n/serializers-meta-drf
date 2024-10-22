6442ced9fcfabd74b0ed0ac5dfbc1df815188fe8
em4n0n <58372945+em4n0n@users.noreply.github.com>
2024-10-21 21:25:36 -0700

    Add Ordering statements

--- LittleLemon/LittleLemonAPI/views.py
+++ LittleLemon/LittleLemonAPI/views.py
@@ -15,12 +15,15 @@ def menu_items(request):
         category_name = request.query_params.get('category')
         to_price = request.query_params.get('to_price')
         search = request.query_params.get('search')
+        ordering = request.query_params.get('ordering')
         if category_name:
             items = items.filter(category__title=category_name)
         if to_price:
             items = items.filter(price=to_price)
         if search:
             items = items.filter(title__icontains=search)
+        if ordering:
+            items = items.order_by(ordering)
         serialized_item = MenuItemSerializer(items, many=True)
         return Response(serialized_item.data)
 
